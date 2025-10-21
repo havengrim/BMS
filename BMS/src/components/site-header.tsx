a5f1e0177f2884d1a4794c1eac8c76531696262d
Angelo <angeloguiao.personal@gmail.com>
2025-10-21 19:44:59 +0800

    remove notification

--- BMS/src/components/site-header.tsx
+++ BMS/src/components/site-header.tsx
@@ -1,7 +1,7 @@
-import { Button } from "@/components/ui/button"
+
 import { Separator } from "@/components/ui/separator"
 import { SidebarTrigger } from "@/components/ui/sidebar"
-import { BellIcon } from "lucide-react"
+
 
 
 export function SiteHeader() {
@@ -14,19 +14,6 @@ export function SiteHeader() {
           className="mx-2 data-[orientation=vertical]:h-4"
         />
         <h1 className="text-base font-medium">Documents</h1>
-        <div className="ml-auto flex items-center gap-2">
-          <Button variant="secondary" asChild size="sm" className="hidden sm:flex">
-            <a
-              href="https://github.com/shadcn-ui/ui/tree/main/apps/v4/app/(examples)/dashboard"
-              rel="noopener noreferrer"
-              target="_blank"
-              className="dark:text-foreground"
-            >
-              <BellIcon />
-              Notification
-            </a>
-          </Button>
-        </div>
       </div>
     </header>
   )
